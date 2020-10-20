6f62e10ce618751b38ffdd9287b6b929c67d1e5d
Vladimir Vargas <vvargasd@unal.edu.co>
2020-10-20 17:06:41 -0500

    login

--- src/main/java/com/example/Alectrion/Service/api/PersonaServiceAPI.java
+++
@@ -1,7 +0,0 @@
-package com.example.Alectrion.Service.api;
-
-import com.example.Alectrion.commons.GenericServiceAPI;
-import com.example.Alectrion.model.Persona;
-public interface PersonaServiceAPI extends GenericServiceAPI<Persona, Long> {
-    
-}
